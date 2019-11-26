fcebd6a3815a741821c152ac3bd2d46014f20ef3
Matteo Fusillo <matt.fusillo@gmail.com>
2019-11-26 15:19:25 +0000

    finished part B

--- bus.rb
+++ bus.rb
@@ -5,6 +5,23 @@ class Bus
   def initialize(route_number, destination)
     @route_number = route_number
     @destination = destination
+    @passengers = []
+  end
+
+  def number_of_passengers
+    return @passengers.length
+  end
+
+  def pick_up(person)
+    @passengers.push(person)
+  end
+
+  def drop_off(person)
+    @passengers.delete(person)
+  end
+
+  def empty()
+    @passengers.clear
   end
 
 end
--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -3,11 +3,13 @@ require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative('../bus')
+require_relative('../person')
 
 class BusTest < MiniTest::Test
 
   def setup
     @bus = Bus.new(44, "Balerno")
+    @person = Person.new("John", 35)
   end
 
   def test_can_get_route_number
@@ -17,5 +19,29 @@ class BusTest < MiniTest::Test
   def test_can_get_destination
     assert_equal("Balerno", @bus.destination)
   end
+
+  def test_number_of_passenger_starts_at_zero
+    assert_equal(0, @bus.number_of_passengers)
+  end
+
+  def test_can_pick_up_person
+    @bus.pick_up(@person)
+    assert_equal(1, @bus.number_of_passengers)
+  end
+
+  def test_can_drop_off_passenger
+    @bus.pick_up(@person)
+    @bus.drop_off(@person)
+    assert_equal(0, @bus.number_of_passengers)
+  end
+
+  def test_can_drop_off_all_passengers
+    @bus.pick_up(@person)
+    @bus.pick_up(@person)
+    @bus.pick_up(@person)
+    @bus.empty
+    assert_equal(0, @bus.number_of_passengers)
+  end
+
 
 end
